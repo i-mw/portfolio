57c55b9222b2c313de600fd2364204eab052609e
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-10 11:56:21 +0200

    Add Footer bar

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="wrap-out">
+      <div className="wrap-in">
+        <div className="container">
+          <p>View this portfolio's source code on 
+              <a href="https://github.com/i-mw/i-mw.github.io">
+              github</a></p><p>
+              &copy;Mustafa Wahba[MW]</p>
+        </div>
+      </div>
+    </footer>    
+  )
+}
+
+export default Footer;
