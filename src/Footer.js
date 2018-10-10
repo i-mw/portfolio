6fd8d760d9866f40d7b1a1bbbc7ff3a73ef347d1
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-10 13:42:48 +0200

    Fix footer to bottom on short body

--- src/Footer.js
+++ src/Footer.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Footer() {
-  return (
-    <footer className="wrap-out">
-      <div className="wrap-in">
-        <div className="container">
-          <p>View this portfolio's source code on 
-              <a href="https://github.com/i-mw/i-mw.github.io">
-              github</a></p><p>
-              &copy;Mustafa Wahba[MW]</p>
+class Footer extends Component {
+  componentDidMount() {
+    this.fixFooterToBottom()
+  }
+
+  fixFooterToBottom() {
+    let footer = document.querySelector('footer');
+    function checkHeights() {
+        let bodyHeight = Number(window.getComputedStyle(document.body)
+            .height.replace('px', ''));
+        let viewportHeight = window.innerHeight;
+
+        if (bodyHeight < viewportHeight) {
+            footer.classList.add('fixed-bottom')
+        } else {
+            footer.classList.remove('fixed-bottom')
+        }
+    }
+    checkHeights();
+    window.addEventListener('resize', checkHeights);
+  }
+
+  render() {
+    return (
+      <footer className="wrap-out">
+        <div className="wrap-in">
+          <div className="container">
+            <p>View this portfolio's source code on 
+                <a href="https://github.com/i-mw/i-mw.github.io">
+                github</a></p><p>
+                &copy;Mustafa Wahba[MW]</p>
+          </div>
         </div>
-      </div>
-    </footer>    
-  )
+      </footer>    
+    )
+  }
 }
 
 export default Footer;
