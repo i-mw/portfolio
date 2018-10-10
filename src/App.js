cf5992746013846bb9b643d03bb18821e50925b3
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-10 19:38:24 +0200

    Join app layout into App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import 'focus-visible';
 import './App.css';
+import AppLayout from './AppLayout.js';
 
 class App extends Component {
   render() {
     return (
-      ''
+      <AppLayout>
+      </AppLayout>
     );
   }
 }
