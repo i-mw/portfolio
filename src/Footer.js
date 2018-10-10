55dafb0a2550ef221a5a98af2412daa0174cef12
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-10 13:43:55 +0200

    style: Add signle non-breaking space

--- src/Footer.js
+++ src/Footer.js
@@ -27,10 +27,10 @@ class Footer extends Component {
       <footer className="wrap-out">
         <div className="wrap-in">
           <div className="container">
-            <p>View this portfolio's source code on 
-                <a href="https://github.com/i-mw/i-mw.github.io">
-                github</a></p><p>
-                &copy;Mustafa Wahba[MW]</p>
+            <p>View this portfolio's source code on &nbsp;
+              <a href="https://github.com/i-mw/i-mw.github.io">github</a></p><p>
+                &copy;Mustafa Wahba[MW]
+            </p>
           </div>
         </div>
       </footer>    
