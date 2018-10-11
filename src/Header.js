c41785250a5cf1139d091a3f6b5851568d873162
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 17:21:24 +0200

    Insert Headline into Header components

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import Headline from './Headline';
 
 function Header(props) {
+  const {headline, headlinePoints, primaryLang} = props;
   return (
-    ''
+    <header className={''}>
+      <div className="container">
+        <Headline headline={headline} headlinePoints={headlinePoints}
+          primaryLang={primaryLang}/>
+      </div>
+    </header>
   );
 }
 
 Header.propTypes = {
+  headline: propTypes.string.isRequired,
+  headlinePoints: propTypes.array,
+  primaryLang: propTypes.string
 }
 
 export default Header;
