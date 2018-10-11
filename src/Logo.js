d8943dd111be0e1597ec95bd4682ddbb1c2eb793
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 15:05:48 +0200

    Add Logo component content

--- src/Logo.js
+++ src/Logo.js
@@ -1,14 +1,18 @@
-import {React} from 'react';
+import React from 'react';
 import propTypes from 'prop-types';
 
 function Logo(props) {
   return (
-    ''
+    <div className="logo wrap-out">
+      <div className="wrap-in">
+        <pre dangerouslySetInnerHTML={{__html: props.logoText}}></pre>
+      </div>
+    </div>
   );
 }
 
 Logo.propTypes = {
-
+  logoText: propTypes.string.isRequired
 }
 
 export default Logo;
