67b5fda6fc4fd360c2acab61a6407e32da944fa5
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 22:29:09 +0200

    Remove trial code

--- src/App.js
+++ src/App.js
@@ -2,28 +2,11 @@ import React, { Component } from 'react';
 import 'focus-visible';
 import './App.css';
 import AppLayout from './AppLayout.js';
-import SearchInput from './SearchInput.js';
-import SearchKeywords from './SearchKeywords.js';
-
-const 
-  keywords = [
-  "front-end development",
-  "data analysis",
-  "react"
-],
-  searchTerm = "data analysis",
-  changeSearchTerm = newValue => {console.log(newValue)};
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
-        <SearchInput colType="projects"
-          searchTerm={searchTerm} changeSearchTerm={changeSearchTerm}
-        />
-
-        <SearchKeywords keywords={keywords} searchTerm={searchTerm}
-          changeSearchTerm={changeSearchTerm}/>
       </AppLayout>
     );
   }
