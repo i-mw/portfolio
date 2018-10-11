67ac400179349c820516f51420f4eeb02ba77dcf
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 15:46:21 +0200

    Bootstrap personal photo's component

---
+++ src/PersonalPhoto.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function PeronalPhoto(props) {
+  return (
+    ''
+  );
+}
+
+PeronalPhoto.propTypes = {
+
+}
+
+export default PeronalPhoto;
