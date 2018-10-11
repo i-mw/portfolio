c06bbddfa1553255f1908b6c321cecdaef4418dd
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 22:00:11 +0200

    Render search keywords

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import 'focus-visible';
 import './App.css';
 import AppLayout from './AppLayout.js';
 import SearchInput from './SearchInput.js';
+import SearchKeywords from './SearchKeywords.js';
+
+const keywords = [
+  "front-end development",
+  "data analysis",
+  "react"
+];
 
 class App extends Component {
   render() {
@@ -11,6 +18,8 @@ class App extends Component {
         <SearchInput colType="projects" searchTerm="hello" changeSearchTerm={searchBox => {
           console.log(searchBox.value)
         }}/>
+
+        <SearchKeywords keywords={keywords}/>
       </AppLayout>
     );
   }
--- src/SearchKeywords.js
+++ src/SearchKeywords.js
@@ -2,12 +2,23 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 function SearchInput(props) {
+  const {keywords} = props;
+
   return (
-    ''
+    <ul className="search-keywords">
+      { 
+        keywords.map(keyword => (
+          <li className="wrap-out" key={keyword}>
+            <p className="wrap-in" tabIndex="0">{keyword}</p>
+          </li>
+        ))
+      }
+    </ul>
   );
 }
 
 SearchInput.propTypes = {
+  keywords: propTypes.array.isRequired
 }
 
 export default SearchInput;
