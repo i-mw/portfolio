0d4fd67f756d1dad1c2d4cb06c43d6abc4524bdb
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 20:20:21 +0200

    Fill AboutSection component's content

--- src/AboutSection.js
+++ src/AboutSection.js
@@ -3,11 +3,18 @@ import propTypes from 'prop-types';
 
 function AboutSection(props) {
   return (
-    ''
+    <li className="details wrap-out">
+      <div className="wrap-in thick-border">
+        {props.sectionContent.content.map((paragraph, index) => {
+          return <p key={index} dangerouslySetInnerHTML={{__html: paragraph.i}}></p>
+        })}
+      </div>
+    </li>
   );
 }
 
 AboutSection.propTypes = {
+  sectionContent: propTypes.object.isRequired
 }
 
 export default AboutSection;
