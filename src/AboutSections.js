67d413d3c0e87daf5fd5690657ad7a6a5070ab79
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 20:23:19 +0200

    Bootstrap About Sections -wrapper- component

---
+++ src/AboutSections.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import AboutSections from './AboutSections.js'
+
+function AboutSections(props) {
+  return (
+    ''
+  );
+}
+
+AboutSections.propTypes = {
+}
+
+export default AboutSections;
