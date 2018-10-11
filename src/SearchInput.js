eb168b07c218855c2013047a008989f22144e771
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 21:17:45 +0200

    Bootstrap Search Input component

---
+++ src/SearchInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function SearchInput(props) {
+  return (
+    ''
+  );
+}
+
+SearchInput.propTypes = {
+}
+
+export default SearchInput;
