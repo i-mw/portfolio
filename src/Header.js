4bd2dd5bd850986ebdb7ff8425127a9479f3f708
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 17:29:50 +0200

    Insert GoToParent component into Header

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import Headline from './Headline';
+import GoToParent from './GoToParent';
 
 function Header(props) {
-  const {headline, headlinePoints, primaryLang} = props;
+  const {headline, headlinePoints, primaryLang, parentCollection} = props;
   return (
     <header className={''}>
       <div className="container">
+        {parentCollection && <GoToParent parentCollection={parentCollection}/>}
         <Headline headline={headline} headlinePoints={headlinePoints}
           primaryLang={primaryLang}/>
       </div>
@@ -17,7 +19,8 @@ function Header(props) {
 Header.propTypes = {
   headline: propTypes.string.isRequired,
   headlinePoints: propTypes.array,
-  primaryLang: propTypes.string
+  primaryLang: propTypes.string,
+  parentCollection: propTypes.string
 }
 
 export default Header;
