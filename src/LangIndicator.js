063b84ba9e87e1352d188c38558ed6dcebe6dd59
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 16:06:37 +0200

    Bootstrap language indicator component

---
+++ src/LangIndicator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function LangIndicator(props) {
+  return (
+    ''
+  );
+}
+
+LangIndicator.propTypes = {
+
+}
+
+export default LangIndicator;
