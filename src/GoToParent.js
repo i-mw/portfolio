304c50d85c264f12ad84675e9c1717775b97f0ca
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 17:01:24 +0200

    Add content to GoToParent component

--- src/GoToParent.js
+++ src/GoToParent.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import backArrow from './back-arrow.svg';
 
 function GoToParent(props) {
   return (
-    ''
+    <div className="go-to-parent">
+      <Link to={"./" + props.parentCollection}>
+        <img src={backArrow} alt="back arrow icon"/>
+        <p>all {props.parentCollection}</p>
+      </Link>
+    </div>
   );
 }
 
 GoToParent.propTypes = {
+  parentCollection: propTypes.string.isRequired
 }
 
 export default GoToParent;
