1d8b931982929bb90f4f80f00244fb3b8e1a598b
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 16:13:54 +0200

    Add content to language indicator component

--- src/LangIndicator.js
+++ src/LangIndicator.js
@@ -3,12 +3,13 @@ import propTypes from 'prop-types';
 
 function LangIndicator(props) {
   return (
-    ''
+    <div className={"lang-indicator " + props.primaryLanguage}
+      title={ props.primaryLanguage + " is the primary language in this project"}></div>
   );
 }
 
 LangIndicator.propTypes = {
-
+  primaryLanguage: propTypes.string.isRequired
 }
 
 export default LangIndicator;
