a6d78f4153a245a3f12107635a5303d33d61798a
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 21:27:30 +0200

    Add SearchInput basic content

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import 'focus-visible';
 import './App.css';
 import AppLayout from './AppLayout.js';
+import SearchInput from './SearchInput.js';
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
+        <SearchInput colType="projects" searchTerm="hello"/>
       </AppLayout>
     );
   }
--- src/SearchInput.js
+++ src/SearchInput.js
@@ -2,12 +2,23 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 function SearchInput(props) {
+  const {colType, searchTerm} = props;
+
   return (
-    ''
+    <div className="search wrap-out">
+      <div className="wrap-in">
+        <input placeholder={"search my " + colType} type="text" name="search"
+          defaultValue={searchTerm}
+        />
+        <button>Search</button>
+      </div>
+    </div>
   );
 }
 
 SearchInput.propTypes = {
+  colType: propTypes.string.isRequired,
+  searchTerm: propTypes.string.isRequired,
 }
 
 export default SearchInput;
