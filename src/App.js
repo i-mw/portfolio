ebbe5f8f5301a63265f887beafbd0f7d316a4eda
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 22:10:00 +0200

    Highlight keyword matching search value

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import AppLayout from './AppLayout.js';
 import SearchInput from './SearchInput.js';
 import SearchKeywords from './SearchKeywords.js';
 
-const keywords = [
+const 
+  keywords = [
   "front-end development",
   "data analysis",
   "react"
-];
+],
+  searchTerm = "data analysis",
+  changeSearchTerm = searchBox => {console.log(searchBox.value)};
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
-        <SearchInput colType="projects" searchTerm="hello" changeSearchTerm={searchBox => {
-          console.log(searchBox.value)
-        }}/>
+        <SearchInput colType="projects"
+          searchTerm={searchTerm} changeSearchTerm={changeSearchTerm}
+        />
 
-        <SearchKeywords keywords={keywords}/>
+        <SearchKeywords keywords={keywords} searchTerm={searchTerm}/>
       </AppLayout>
     );
   }
--- src/SearchKeywords.js
+++ src/SearchKeywords.js
@@ -2,14 +2,16 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 function SearchInput(props) {
-  const {keywords} = props;
+  const {keywords, searchTerm} = props;
 
   return (
     <ul className="search-keywords">
       { 
         keywords.map(keyword => (
           <li className="wrap-out" key={keyword}>
-            <p className="wrap-in" tabIndex="0">{keyword}</p>
+            <p className={
+              keyword === searchTerm ? "wrap-in active-keyword" : "wrap-in"
+              } tabIndex="0">{keyword}</p>
           </li>
         ))
       }
@@ -18,7 +20,9 @@ function SearchInput(props) {
 }
 
 SearchInput.propTypes = {
-  keywords: propTypes.array.isRequired
+  keywords: propTypes.array.isRequired,
+  searchTerm: propTypes.string.isRequired,
+
 }
 
 export default SearchInput;
