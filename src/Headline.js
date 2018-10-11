ff53320059640bf11e56f61af8e8d3d1d905d931
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 16:41:14 +0200

    Add headline component's content

--- src/Headline.js
+++ src/Headline.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import LangIndicator from './LangIndicator'
 
 function Headline(props) {
   return (
-    ''
+    <div className="wrap-out headline">
+      <div className="wrap-in">
+        <h1>{props.headline}</h1>
+        {
+          props.primaryLang && <LangIndicator primaryLang={"javascript"}/>
+        }
+        {
+          props.headlinePoints && (
+              props.headlinePoints[0].content.map(
+                (point, index) => <p key={index}>{point.i}</p>
+              )
+          )
+        }
+    </div>
+</div>
   );
 }
 
 Headline.propTypes = {
-
+  headline: propTypes.string.isRequired,
+  headlinePoints: propTypes.array,
+  primaryLang: propTypes.string
 }
 
 export default Headline;
