6ddb69ed560366f8f91e2ddd2f9cde22b85da25f
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 20:33:19 +0200

    insert individual about sections into AboutSections

--- src/AboutSections.js
+++ src/AboutSections.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import AboutSections from './AboutSections.js'
+import AboutSection from './AboutSection.js'
 
 function AboutSections(props) {
   return (
-    ''
+    <ul>
+      {props.details.map((sectionContent, index) => {
+        return <AboutSection key={index} sectionContent={sectionContent}/>
+      })}
+    </ul>
   );
 }
 
 AboutSections.propTypes = {
+  details: propTypes.array.isRequired
 }
 
 export default AboutSections;
