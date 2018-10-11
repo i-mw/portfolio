b93ff0dd3e62aa384430f46c3e1237e71ee5a01a
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 20:19:06 +0200

    Bootstrap About Section's component

---
+++ src/AboutSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function AboutSection(props) {
+  return (
+    ''
+  );
+}
+
+AboutSection.propTypes = {
+}
+
+export default AboutSection;
