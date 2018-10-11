e7b35c6466fa2c9ddfcd36698dfb551001408211
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-11 18:27:56 +0200

    Insert Logo component into Header

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,21 @@ import React from 'react';
 import propTypes from 'prop-types';
 import Headline from './Headline';
 import GoToParent from './GoToParent';
+import Logo from './Logo';
 
 function Header(props) {
-  const {headline, headlinePoints, primaryLang, parentCollection} = props;
+  const {
+    headline,
+    headlinePoints,
+    primaryLang,
+    parentCollection,
+    logoText
+  } = props;
   return (
     <header className={''}>
       <div className="container">
         {parentCollection && <GoToParent parentCollection={parentCollection}/>}
+        {logoText && <Logo logoText={logoText}/>}
         <Headline headline={headline} headlinePoints={headlinePoints}
           primaryLang={primaryLang}/>
       </div>
@@ -20,7 +28,8 @@ Header.propTypes = {
   headline: propTypes.string.isRequired,
   headlinePoints: propTypes.array,
   primaryLang: propTypes.string,
-  parentCollection: propTypes.string
+  parentCollection: propTypes.string,
+  logoText: propTypes.string
 }
 
 export default Header;
