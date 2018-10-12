a0949db46bbf077c8e491a03bf7dae53a3deaadb
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-12 10:28:07 +0200

    Add content to Jumbo component

--- src/Jumbo.js
+++ src/Jumbo.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import Summary from './Summary';
 
 function Jumbo(props) {
+  const {jumboImage, summary} = props;
   return (
-    ''
+    <section className="wrap-out jumbotron">
+      <div className="wrap-in">
+        <div className="screenshot">
+          <a tabIndex="-1" href={jumboImage.url}
+            style={{backgroundImage: "url(" + jumboImage.url + ")"}}
+            target="_blank" title="jumbo image">jumbo image</a>
+        </div>
+        <Summary summaryContent={summary[0].content}/>
+      </div>
+    </section>
   );
 }
 
 Jumbo.propTypes = {
+  jumboImage: propTypes.object.isRequired,
+  summary: propTypes.array.isRequired
 }
 
 export default Jumbo;
