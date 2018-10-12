20fd25c1f97f6a08748ec580e8e6d978026fe9b7
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-12 08:36:27 +0200

    Insert Doc Cards into Search Results' component

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import DocCard from './DocCard'
 
 function SearchResults(props) {
+  const {filteredDocs, parentCol} = props;
+
   return (
-    ''
+    <ul className="search-results">
+    {
+      filteredDocs.map(docData => (
+        <DocCard docData={docData} parentCol={parentCol} key={docData.id}/>
+      ))
+    }
+    </ul>
   );
 }
 
 SearchResults.propTypes = {
+  filteredDocs: propTypes.array.isRequired,
+  parentCol: propTypes.string.isRequired
 }
 
 export default SearchResults;
