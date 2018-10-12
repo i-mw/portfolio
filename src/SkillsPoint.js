e1802e000f888bc50fee46d58993fb331c6ada0e
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-12 20:12:48 +0200

    Add SkillsPoint component

---
+++ src/SkillsPoint.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function SkillsPoint(props) {
+  let skills = [];
+  for (let skill in props.skills) skills.push(props.skills[skill]);
+  return (
+        <tr>
+          <td>Skills:</td>
+          {
+            skills.map(skill => (
+              <td key={skill.id}><a href={'./skills/' + skill.id}>{skill.title}</a></td>
+            ))
+          }
+        </tr>
+  )
+}
+
+SkillsPoint.propTypes = {
+  skills: propTypes.object.isRequired
+}
+
+export default SkillsPoint;
