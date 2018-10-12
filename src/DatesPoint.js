6e22c8547ce48e4cff136d9ca4167bf4c5ad8709
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-12 18:35:30 +0200

    Add DatesPoint component

---
+++ src/DatesPoint.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function DatesPoint(props) {
+  const {dates} = props;
+
+  return (
+        <tr>
+          <td>Year:</td>
+          <td>{dates.endedAt ? dates.endedAt.getFullYear() : 'working on'}</td>
+        </tr>
+  )
+}
+
+DatesPoint.propTypes = {
+  dates: propTypes.object.isRequired
+}
+
+export default DatesPoint;
