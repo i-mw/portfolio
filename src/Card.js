465e05702fe83478b4450b7f3fc0d98ecb01d0fd
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-12 08:09:34 +0200

    Fill Cards's component content

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import LangIndicator from './LangIndicator';
+import {Link} from 'react-router-dom'
 
 function Card(props) {
+  const {docData, parentCol} = props;
+
   return (
-    ''
+    <li className="wrap-out current">
+      <Link to={'./' + parentCol + '/'+ docData.id} className="wrap-in">
+        {
+          docData.images && 
+          <div className="image-container"
+            style={{backgroundImage: "url('" + docData.images
+              .find(image => image.placing === 'thumbnail').url + "')"}}></div>
+        }
+        <div className="text">
+            <p>{docData.title}</p>
+            <LangIndicator primaryLang={docData.primaryLanguage}/>
+        </div>
+      </Link>
+    </li> 
   );
 }
 
 Card.propTypes = {
+  docData: propTypes.object.isRequired,
+  parentCol: propTypes.string.isRequired
 }
 
 export default Card;
