23cd071d5eac81a7aa6d9ea910a441f1096dbdd8
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-12 21:28:28 +0200

    Remove trial code

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import React, { Component } from 'react';
 import 'focus-visible';
 import './App.css';
 import AppLayout from './AppLayout.js';
-import RemoteDataPoint from './RemoteDataPoint'
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
-        <RemoteDataPoint
-          parentCollection={'skills'} searchCollection={'projects'}
-          searchProperty={'skills.html.id'} searchValue={'html'}/>
       </AppLayout>
     );
   }
