65f958a4891864da3e18e30a28a7301083f8cb08
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-12 08:17:21 +0200

    Bootsrap Search Resutls' component

---
+++ src/SearchResults.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function SearchResults(props) {
+  return (
+    ''
+  );
+}
+
+SearchResults.propTypes = {
+}
+
+export default SearchResults;
