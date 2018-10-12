4d0dd6cc4778fe146ffed9244e4d733c3909f115
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-12 09:23:32 +0200

    Add summary's component conent

--- src/Summary.js
+++ src/Summary.js
@@ -3,11 +3,16 @@ import propTypes from 'prop-types';
 
 function Summary(props) {
   return (
-    ''
+    <div className="summary">
+        {props.summaryContent.map((paragraph, index) => {
+          return <p key={index} dangerouslySetInnerHTML={{__html: paragraph.i}}></p>
+        })}
+    </div>   
   );
 }
 
 Summary.propTypes = {
+  summaryContent: propTypes.array.isRequired
 }
 
 export default Summary;
