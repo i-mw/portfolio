4c0bd27620819f19b140893b7a3cf51c88aa8a83
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-14 10:03:15 +0200

    Add LinksTable component

---
+++ src/LinksTable.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import PreviewPoint from './PreviewPoint'
+import SourcePoint from './SourcePoint'
+import CertificatePoint from './CertificatePoint'
+import SyllabusPoint from './SyllabusPoint'
+
+function LinksTable(props) {
+  const {
+    preview,
+    source,
+    certificate,
+    syllabus
+  } = props;
+
+  return (
+    <section className="wrap-out points">
+      <div className="wrap-in">
+        <table>
+          <tbody>
+            {preview && <PreviewPoint preview={preview}/>}
+            {source && <SourcePoint source={source}/>}
+            {certificate && <CertificatePoint certificate={certificate}/>}
+            {syllabus && <SyllabusPoint syllabus={syllabus}/>}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  )
+}
+
+LinksTable.propTypes = {
+  preview: propTypes.string,
+  source: propTypes.string,
+  certificate: propTypes.string,
+  syllabus: propTypes.string,
+}
+
+export default LinksTable;
