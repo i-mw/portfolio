8215e417f3405b1e1a0f37da81de215e780dab98
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-18 18:36:48 +0200

    rename a prop

--- src/DocCard.js
+++ src/DocCard.js
@@ -4,14 +4,14 @@ import LangIndicator from './LangIndicator';
 import {Link} from 'react-router-dom'
 
 function DocCard(props) {
-  const {docData, parentCol} = props;
+  const {docData, colType} = props;
   const defaultImage = 'https://firebasestorage.googleapis.com/v0/b/' + 
     'portfolio-v2-9ca05.appspot.com/o/placeholder.png?' +
     'alt=media&token=97415612-b965-4f82-b608-adf77e574707';
 
   return (
     <li className={docData.dates.endedAt ? "wrap-out" : "wrap-out current"}>
-      <Link to={'./' + parentCol + '/'+ docData.id} className="wrap-in">
+      <Link to={'./' + colType + '/'+ docData.id} className="wrap-in">
         {
           docData.images && 
           <div className="image-container"
@@ -29,7 +29,7 @@ function DocCard(props) {
 
 DocCard.propTypes = {
   docData: propTypes.object.isRequired,
-  parentCol: propTypes.string.isRequired
+  colType: propTypes.string.isRequired
 }
 
 export default DocCard;
