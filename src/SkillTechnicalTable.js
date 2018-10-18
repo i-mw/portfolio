20a0f07f3912b6781a43c5697993fda2e03377ce
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-18 13:55:41 +0200

    Add SkillTechnicalTable component

---
+++ src/SkillTechnicalTable.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import RemoteDatPoint from './RemoteDataPoint'
+
+function SkillTechnicalTable(props) {
+  return (
+    <section className="wrap-out points">
+      <div className="wrap-in">
+        <table>
+          <tbody>
+            <RemoteDataPoint
+              parentCollection='skills'
+              searchCollection='projects'
+              searchProperty={'skills.' + documentId + '.id'}
+              searchValue={documentId}
+            />
+            <RemoteDataPoint
+              parentCollection='skills'
+              searchCollection='snippets'
+              searchProperty={'skills.' + documentId + '.id'}
+              searchValue={documentId}
+            />
+            <RemoteDataPoint
+              parentCollection='skills'
+              searchCollection='courses'
+              searchProperty={'skills.' + documentId + '.id'}
+              searchValue={documentId}
+            />
+            <RemoteDataPoint
+              parentCollection='skills'
+              searchCollection='readings'
+              searchProperty={'skills.' + documentId + '.id'}
+              searchValue={documentId}
+            />
+          </tbody>
+        </table>
+      </div>
+    </section>
+  )
+}
+
+SkillTechnicalTable.propTypes = {
+  documentId: propTypes.string.isRequired
+}
+
+export default SkillTechnicalTable;
