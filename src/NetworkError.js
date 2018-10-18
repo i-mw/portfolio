15a7fed4b352a9c21300443dd3247dddf1c45fe7
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-18 15:43:43 +0200

    Add network error component

---
+++ src/NetworkError.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NetworkError(props) {
+  return (
+    <p className="network-error">Network error! Check your connection.</p>
+  )
+}
+
+export default NetworkError;
