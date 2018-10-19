f180f3fe2c9f8b6f33b527fe0d110370dcb13aab
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-19 09:49:38 +0200

    fix: Retrieve skills from data base without ordering

--- src/dbAPI.js
+++ src/dbAPI.js
@@ -4,13 +4,24 @@ export const getDoc = (collection, document) =>
   db.collection(collection).doc(document).get()
   .then(doc => doc.data());
 
-export const getCollection = collection =>
-  db.collection(collection).orderBy('dates.endedAt', 'desc').get()
-  .then(querySnapshot => {
-    let list = [];
-    querySnapshot.forEach(doc => {list.push(doc.data())});
-    return list;
-  });
+export const getCollection = collection => {
+  if (collection.indexOf('skills') > -1) {
+    return db.collection(collection).get()
+    .then(querySnapshot => {
+      let list = [];
+      querySnapshot.forEach(doc => {list.push(doc.data())});
+      return list;
+    });
+  } 
+  else {
+    return db.collection(collection).orderBy('dates.startedAt', 'desc').get()
+    .then(querySnapshot => {
+      let list = [];
+      querySnapshot.forEach(doc => {list.push(doc.data())});
+      return list;
+    });
+  }
+}
 
 export const getCustomCollection = (collection, propertyName, propertyValue) => 
   db.collection(collection).where(propertyName, '==', propertyValue)
