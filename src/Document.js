393565997d7136864270b48fc712efd97f538356
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-19 13:31:23 +0200

    Insert tables into Document component

--- src/Document.js
+++ src/Document.js
@@ -3,6 +3,9 @@ import propTypes from 'prop-types';
 import * as dbAPI from './dbAPI.js';
 import Header from './Header';
 import Jumbo from './Jumbo';
+import RoutineTable from './RoutineTable';
+import LinksTable from './LinksTable';
+import TechnicalTable from './TechnicalTable'
 
 class Document extends Component {
   state = {
@@ -41,6 +44,25 @@ class Document extends Component {
             <div className="container" tabIndex="-1">
               <Jumbo summary={doc.summary}
                 jumboImage={doc.images.find(image => image.placing === 'jumbo')}/>
+              <RoutineTable
+                primaryLanguage={doc.primaryLanguage}
+                categories={doc.categories}
+                dates={doc.dates}
+                duration={doc.duration}
+                provider={doc.provider}
+                author={doc.author}
+                publisher={doc.publisher}
+                partOf={doc.partOf}/>
+              <LinksTable
+                preview={doc.links.preview}
+                source={doc.links.source}
+                certificate={doc.links.certificate}
+                syllabus={doc.links.syllabus}/>
+              <TechnicalTable
+                parentCollection={this.props.parentCollection}
+                documentId={this.props.documentId}
+                skills={doc.skills}
+                />
             </div>
           </main>
         </section>
