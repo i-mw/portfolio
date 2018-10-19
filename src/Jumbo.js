4b0e10d37a19fd29812ca24d67763b879539fef7
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-19 13:00:27 +0200

    Fix default image

--- src/Jumbo.js
+++ src/Jumbo.js
@@ -4,19 +4,22 @@ import Summary from './Summary';
 
 function Jumbo(props) {
   const {jumboImage, summary} = props;
+
+  const retrievedImage = jumboImage && jumboImage.url;
   const defaultImage = 'https://firebasestorage.googleapis.com/v0/b/' + 
     'portfolio-v2-9ca05.appspot.com/o/placeholder.png?' +
     'alt=media&token=97415612-b965-4f82-b608-adf77e574707';
+  const image = retrievedImage || defaultImage;
 
   return (
     <section className="wrap-out jumbotron">
       <div className="wrap-in">
         <div className="screenshot">
-          <a tabIndex="-1" href={jumboImage.url}
-            style={{backgroundImage: "url(" + jumboImage.url || defaultImage + ")"}}
+          <a tabIndex="-1" href={image}
+            style={{backgroundImage: "url(" + image + ")"}}
             target="_blank" title="jumbo image">jumbo image</a>
         </div>
-        {summary[0].content && <Summary summaryContent={summary[0].content}/>}
+        {summary && <Summary summaryContent={summary[0].content}/>}
       </div>
     </section>
   );
