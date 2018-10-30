ab5f42131dfe1f1560287acc1ed8d8b077e5b491
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-10-30 22:16:17 +0200

    Add basic Routes to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
 import 'focus-visible';
 import './App.css';
 import AppLayout from './AppLayout.js';
-import NotFound from './NotFound'
+import NotFound from './NotFound';
+import About from './About';
+import Collection from './Collection'
+import Document from './Document'
 
 class App extends Component {
   render() {
+    const collections = ['projects', 'snippets', 'skills',
+      'courses', 'certificates', 'readings'];
+
     return (
       <AppLayout>
-        <NotFound/>
+        <Switch>
+          <Route exact path="/" component={About}/>
+          {
+            collections.map(col => (
+              <Route exact path={'/' + col} render={_=> <Collection colType={col} />}/>
+            ))
+          }
+          <Route component={NotFound}/>
+        </Switch>
       </AppLayout>
     );
   }
