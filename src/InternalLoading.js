898fb6f6513118cf90618567387113da74691df5
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2019-09-14 21:07:15 +0200

    Convert loading icons into inline svg

--- src/InternalLoading.js
+++ src/InternalLoading.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import loadingIcon from './loading.svg'
 
 /* This loading component and icon is floating after content
    And corresponds to loading parts or elements in a page, 
@@ -13,7 +12,30 @@ function InternalLoading(props) {
                                       marginBottom: "100px",
                                       left: "calc(50% - 35px)"}}>
       <div className="wrap-in" style={{border: 0, padding: 0}}>
-        <img src={loadingIcon} alt="loading icon" style={{width: "70px"}}/>
+        <svg className="prefix__ldt-blank" width={70} height={19}
+            preserveAspectRatio="xMidYMid" style={{background: '0 0'}} {...props}>
+          <text fill="#435472" transform="translate(0 19)"
+                fontSize={32} className="ld ld-blink"
+                style={{animationDelay: "-0.7s", animationDuration: "2s",
+                        animationDirection: "normal",
+                        transformOrigin: "16.9062px 0px 0px"}}>
+            {'\u25A0'}
+          </text>
+          <text fill="#435472" transform="translate(25 19)"
+                fontSize={32} className="ld ld-blink"
+                style={{animationDelay: "-0.35s", animationDuration: "2s",
+                animationDirection: "normal",
+                transformOrigin: "16.9062px 0px 0px"}}>
+            {'\u25A0'}
+          </text>
+          <text fill="#435472" transform="translate(51 19)"
+                fontSize={32} className="ld ld-blink"
+                style={{animationDelay: "0s", animationDuration: "2s",
+                animationDirection: "normal",
+                transformOrigin: "16.9062px 0px 0px"}}>
+            {'\u25A0'}
+          </text>
+        </svg>
       </div>
     </div>
   )
