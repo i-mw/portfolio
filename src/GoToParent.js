0bf9442041ff3424ebf60e0c545a38c9cb7b6443
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2019-10-04 15:11:55 +0200

    Convert svg icons into inline jsx

--- src/GoToParent.js
+++ src/GoToParent.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import {Link} from 'react-router-dom';
-import backArrow from './back-arrow.svg';
 
 function GoToParent(props) {
   return (
     <div className="go-to-parent">
       <Link to={"/" + props.parentCollection}>
-        <img src={backArrow} alt="back arrow icon"/>
+        <svg fill="#435472" height={22} viewBox="0 0 24 24" width={22}><path d="M0 0h24v24H0z" fill="none" /><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" /></svg>
         <p>all {props.parentCollection}</p>
       </Link>
     </div>
